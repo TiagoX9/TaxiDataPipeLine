d1c1bbf9e81af25b7edc8b391058c4fce2696966
sushma-goutam <sushma.goutam@perkinelmer.com>
2020-02-12 16:43:36 +0100

    Minor clean-up

--- taxidata/data_processor.py
+++ taxidata/data_processor.py
@@ -1,8 +1,7 @@
 """
-This module provides analysis of NYC Yellow Taxi data
+This module provides analysis of NYC Yellow Taxi data.
 """
 
-import argparse
 import logging
 
 import dask.dataframe as dd
@@ -14,14 +13,18 @@ logger.setLevel(logging.INFO)
 
 
 def initialize():
-    fileHandler = logging.FileHandler('pipeline.log')
-    fileHandler.setLevel(logging.INFO)
+    """
+    Enable logging to a file and to console.
+    :return:
+    """
+    file_handler = logging.FileHandler('pipeline.log')
+    file_handler.setLevel(logging.INFO)
 
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
 
-    logger.addHandler(fileHandler)
-    logger.addHandler(consoleHandler)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
 
 def clean_data(df, index):
@@ -48,7 +51,7 @@ def clean_data(df, index):
 
 def read_trip_data(url):
     """
-    Loads CSV file in chunks of 100 MB and return dataframe
+    Loads CSV file in chunks of 100 MB and return dataframe.
     :param url: url for loading csv data
     :return: dataframe
     """
@@ -68,7 +71,7 @@ def read_trip_data(url):
 
 def calculate_monthly_average(df):
     """
-    Calculates average trip length per month
+    Calculates average trip length per month.
     :param df: dataframe
     :return: Average trip length per month
     """
